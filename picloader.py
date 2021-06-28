6bb9d43cb6ca05da3e2495c06b85a0673cd32980
Adrian Crombach <adrian.c@t-online.de>
2021-06-28 21:55:30 +0200

    added readme.md

--- picloader.py
+++ picloader.py
@@ -1,35 +1,49 @@
 import io
+from os import access
 import sys
 import optparse
 import urllib
 import requests
 
+from PIL import Image
 from tqdm import tqdm
 from bs4 import BeautifulSoup as bs
 
 
 def main(argv):
+
     url = argv[1]
     savepath = None
 
     parser = optparse.OptionParser()
     parser.add_option('-o',action="store") # -o : Output Directory
-    parser.add_option('-a',action="store") # -a : Get all types of Images
+    parser.add_option('-a',action="store_true",dest="a") # -a : Get all types of Images
+    parser.add_option('-j',action="store_true",dest="j") # -j : Get all jpeg Images
+    parser.add_option('-p',action="store_true",dest="p") # -p : Get all png Images
 
     options, remainder = parser.parse_args()
 
     savepath = options.o
 
-    print("got: " + url)
+    print("getting images from: " + url)
     img_url_list = get_img_urls(url)
-    print("downloading...")
 
-    for img_url in img_url_list:
-        try: 
-            if img_url.find(".jpg") != -1 or img_url.find(".png") != -1:
-                dl_raw(img_url,savepath)
-        except Exception as e:
-            print(e)
+    try: 
+        if options.a:
+            print("downloading images")
+            dl_all(img_url_list,savepath)
+
+        else:
+            if options.j:
+                print("downloading JPEGs")
+                dl_jpg(img_url_list,savepath)
+
+            if options.p:
+                print("downloading PNGs")
+                dl_png(img_url_list,savepath)
+
+    except Exception as e:
+        print(e)
 
 
 # Extract all absolute image-source-urls from the html code of given webpage url,
@@ -48,7 +62,6 @@ def get_img_urls(url):
         img_urls.append(img_url)
         #print("URL: {}".format(img_url))
 
-
     print("Found {} Images".format(len(img_urls)))
 
     return img_urls
@@ -57,9 +70,25 @@ def get_img_urls(url):
 # Download image from url leading to image file and save them in given path 
 # (if savepath=None: Save in current working directory)
 
+def dl_jpg(img_urls,savepath):
+    for url in img_urls:
+        if url.find(".jpg") != -1:
+            dl_raw(url,savepath)
+
+
+
+def dl_png(img_urls,savepath):
+    for url in img_urls:
+        if url.find(".png") != -1:
+            dl_raw(url,savepath)
+
+
+def dl_all(img_urls,savepath):
+    for url in img_urls:
+        dl_raw(url,savepath)
+
 def dl_raw(img_url,savepath):
     resp = requests.get(img_url,stream=True)
-    img_b = io.BytesIO(resp.content)
 
     if savepath == None:
         if img_url.find('/'):
@@ -71,8 +100,6 @@ def dl_raw(img_url,savepath):
     with open(fpath,"wb") as f:
         print("Writing to: {}".format(savepath+img_url.rsplit('/', 1)[1],"wb") )
         f.write(resp.content)
-        
-
 
 if __name__ == "__main__":
     main(sys.argv)
---
+++ readme.md
@@ -0,0 +1,10 @@
+# WebPicDL
+A simple command line tool for downloading all .jpg and .png images from a webpage, written in python.
+
+## How to Use
+Open the command line and call picloader.py with the first argument being the target URL
+    picloader.py http://www.exampleurl...
+All pictures will be saved in the current working directory.
+
+The savepath can be changed by adding attribute -o 
+    picloader.py http://www.exampleurl... -o C:/Exampledir/
